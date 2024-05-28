e364d9ec4f778207ec2d5644e60d0e9cda7a0fc5
hoangtyit <hoangty.it@gmail.com>
2024-05-28 15:15:50 +0700

    rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qypjemfmrgjrxkmfyspscdnqsshjr8v5qsuc7drl7vw2fjpaugp2kqq7xmpf3 -r dero-node.mysrv.cloud:10100 -p rpc;
+    ./astrominer -w dero1qypjemfmrgjrxkmfyspscdnqsshjr8v5qsuc7drl7vw2fjpaugp2kqq7xmpf3 -r dero-node.mysrv.cloud:10100 -m 7 -p rpc;
     sleep 5;
 done
